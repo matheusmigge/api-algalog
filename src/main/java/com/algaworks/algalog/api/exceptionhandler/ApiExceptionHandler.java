9412227c0bdb16192e2563148dd77032eeb8c896
Matheus Migge <matheusmigge@gmail.com>
2023-01-07 16:58:24 -0300

    implemented error messages in the exception handler

--- src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,20 +1,51 @@
 package com.algaworks.algalog.api.exceptionhandler;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @ControllerAdvice 
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private MessageSource messageSource;
     
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
-        return super.handleExceptionInternal(ex, "Valor inválido", headers, status, request);
+            List<Problema.Campo> campos = new ArrayList<>();
+
+            for (ObjectError error : ex.getBindingResult().getAllErrors()){
+
+                // O nome disso abaixo é CASTING. Estudar isso!
+                String nome = ((FieldError) error).getField();
+                String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+
+                campos.add(new Problema.Campo(nome, mensagem));
+
+            }
+
+            Problema problema = new Problema();  
+            problema.setStatus(status.value()); 
+            problema.setDataHora(LocalDateTime.now());
+            problema.setTítulo("Um ou mais campos estão inválidos. Verifique os campos e tente novamente.");
+            problema.setCampos(campos);
+
+        return super.handleExceptionInternal(ex, problema, headers, status, request);
     }
 }
