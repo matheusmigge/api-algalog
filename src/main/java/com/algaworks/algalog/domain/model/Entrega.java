a72e6bf8dafa9fcad83206bf88221b1a951bddbc
Matheus Migge <matheusmigge@gmail.com>
2023-01-12 18:42:37 -0300

    adding delivery validations

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.domain.model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,6 +14,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,18 +32,24 @@ public class Entrega {
 
     // Declara propriamente a ligação com a classe cliente existente.
     @ManyToOne
+    @NotNull
     private Cliente cliente;
     
     // Declara que dentro dessa classe contém algumas variáveis que originalmente pertenceriam a classe atual, porém foram jogadas em uma classe separada para melhor organização.
     @Embedded 
     private Destinatario destinatario;
 
+    @NotNull 
     private BigDecimal taxa;
 
+    @JsonProperty(access = Access.READ_ONLY)
     //  Declara que o enum que aparecerá na coluna será a opção selecionada em String, e não o índice dessa opção (Por exemplo, 1 = PENDENTE. EnumType.STRING mostraria o status PENDENTE ao invés do número 1).
     @Enumerated(EnumType.STRING) 
     private StatusEntrega status;
 
+    @JsonProperty(access = Access.READ_ONLY)
     private LocalDateTime dataPedido;
+
+    @JsonProperty(access = Access.READ_ONLY)
     private LocalDateTime dataFinalizacao;
 }
